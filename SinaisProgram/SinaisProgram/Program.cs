cf932780e5d30208c92141c086134a78ba78f1e6
chiroshe@hotmail.com <chiroshe@hotmail.com>
2018-07-22 10:53:05 -0300

    sdas
    fcamara

---
+++ SinaisProgram/SinaisProgram/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SinaisProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            string readSigns = Console.ReadLine();
+            if (!String.IsNullOrEmpty(readSigns))
+            {
+                var retSigns = ReturnSigns(readSigns);
+                Console.WriteLine(retSigns.ToString());
+            }
+            else
+                Console.WriteLine("Texto nao pode ser vazio");
+
+            Console.ReadKey();
+        }
+        public static bool ReturnSigns(string signs)
+        {
+            Dictionary<char, char> dicChar = new Dictionary<char, char>() ;
+            dicChar.Add('[', ']');
+            dicChar.Add('(', ')');
+            dicChar.Add('{', '}');
+          
+            int countLength = signs.Length;
+            int midCount = countLength / 2;
+            var arrSigns = signs.ToCharArray();
+            int actualCounter = countLength - 1;
+            for (int i = 0; i < midCount-1; i++ )
+            {
+                for (int x = actualCounter; x > midCount; x--)
+                {
+                    if (!dicChar.ContainsKey(arrSigns[i]) || dicChar[arrSigns[i]] != arrSigns[x])
+                    {
+                        return false;
+                    }
+                    actualCounter = x-1;
+                    break;
+                }
+            }
+            return true;
+
+        }
+    }
+}
---
+++ SoldierProgram/SoldierProgram/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoldierProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string _readSoldiers = Console.ReadLine();
+            short _countSoldiers = 0;
+            System.Int16.TryParse(_readSoldiers, out _countSoldiers);
+            if (_countSoldiers != 0)
+            {
+                var _retLiveSoldier = ReturnWhoLives(_countSoldiers);
+                Console.WriteLine(_retLiveSoldier.ToString());
+            }
+            else
+                Console.WriteLine("Programa não funciona, não é um numerico");
+            Console.ReadKey();
+        }
+        public static short ReturnWhoLives(short countSoldiers)
+        {
+            ArrayList arrSoldiers = new ArrayList();
+            for (short i = 1; i <= countSoldiers; i++)
+                arrSoldiers.Add(i);
+
+            int counter = arrSoldiers.Count-1;
+
+            while (arrSoldiers.Count > 1)
+            {
+                counter = arrSoldiers.Count;
+                for (int i=0; i < counter; i++)
+                {
+                    if ( i == counter - 1)
+                    {
+                        arrSoldiers.RemoveAt(0);
+                        break;
+                    }
+                    arrSoldiers.RemoveAt(i + 1);
+                   
+                   counter = arrSoldiers.Count;
+                }
+            }
+            return (short)arrSoldiers[0];
+
+        }
+    }
+}
